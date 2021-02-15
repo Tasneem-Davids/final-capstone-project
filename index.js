10ab2af77bf7052f82b76400de639120bdc2e829
Tasneem Davids <davidstasneem92@gmail.com>
2021-02-15 13:32:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,19 @@ require('dotenv').config();//To enable the use of env files
 app.use(express.json())//Middleware
 app.use(cors())
 app.use(logger('dev'))
-app.use(helmet())
+app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'", "'unsafe-inline'"],
+        scriptSrc: ["'self'", "'unsafe-inline'"],
+        imgSrc: ["'self'", "data:"],
+        frameSrc: ["'self'"],
+        mediaSrc: ["'self'"],
+        objectSrc: ["'self'"],
+        upgradeInsecureRequests: [],
+      },
+    })
+  );
 
 app.use('/auth', require('./routes/auth.route'))//Import routes
 app.use('/appointments', require('./routes/appointments.route'))
